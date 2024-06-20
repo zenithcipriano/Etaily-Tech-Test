0c975f76ff78f4db7c9406b591f1bd7318566837
Zenith Averi Cipriano <zvcipriano@up.edu.ph>
2024-06-20 16:10:04 +0800

    routes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Signup from "./pages/Signup";
 import Users from "./pages/Users";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';  
 
 function App() {
 
